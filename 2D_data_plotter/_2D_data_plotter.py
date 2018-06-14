b5edec526defae75548710e5bdcb4af446d4c772
Torgrim Solvang Stensrud <torgrimss@gmail.com>
2018-06-14 12:50:03 +0200

    Initial commit

--- 2D_data_plotter/_2D_data_plotter.py
+++ 2D_data_plotter/_2D_data_plotter.py
@@ -1,1 +1,33 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from pylab import *
 
+
+
+# LaTex Interpreter
+plt.rc('text', usetex=True)
+plt.rc('font', family='serif')
+
+# Declare x/y as empty arrays
+x, y = [], []
+n = 0.0
+
+# Append data values from file to y
+with open("test_data.txt", "r") as data_buffer:
+    y_raw = []
+    for line in data_buffer:
+        y_raw = line.split()
+        y.append(float(y_raw[0]))
+
+# x-array in increments of 1 with same length as y
+for i in range(len(y)):
+    x.append(n)
+    n += 1.0
+
+# Plot results
+plt.figure(figsize=(9,6))
+plt.plot(x, y, linewidth=3.0, color="orange")
+plt.xlabel("$x$")
+plt.ylabel("$y$")
+plt.title("2D plot from .txt data: $y \in (0,9)$", fontsize=18.0)
+plt.show()
---
+++ README.md
@@ -0,0 +1,5 @@
+#Simple 2D plots from data file
+
+This repos contains a simple way to add data from external files and plot it in a LaTeX-styled figure. Notice that `line.split()` here takes no arguments since each line contains 1 value, but for cases with several values per line simply add the "split"-argument. E.g. `line.split(",")` if values are separated by a comma.
+
+![Plot Figure](2D_data_plotter/plot.png)
